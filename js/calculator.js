1f30df73e934941c814a53c08b3852d18692f1a0
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-09-25 18:16:57 +0200

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+/**
+ * 1. Bind DOM elements to variables
+ */
+
+/**
+ * 2. Bind click event listeners to all different elements in the calculator
+ */
+
+/**
+ * 3. Write the calculation logic between two numbers,
+ * which will become the equals sign click event callback
+ */
+
+/**
+ * 4. Write the operator click event callback
+ */
+
+/**
+ * 5. Write the number click event callback
+ */
